13402e9038923deee88395a2e37efb8a3ef4a45f
shlegel <sveta.shlegel@mail.ru>
2023-06-15 00:58:03 +0300

    added functions to class JSONSever

--- src/sever_cls.py
+++ src/sever_cls.py
@@ -60,6 +60,50 @@ class JSONSever(VacanciesSever):
         else:
             return vacancies
 
+    def get_vacancies_by_employment(self, employment):
+        """Возвращает вакансии, соответствующие выбранному типу занятости"""
+        if employment == 1:
+            emp_criterion = ['Полный рабочий день', 'Полная занятость']
+        elif employment == 2:
+            emp_criterion = ['Неполная дистанционная занятость', 'Частичная занятость']
+        vacancies = []
+        with open(self.file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if not data:
+                print(f"По запросу '{self.vacancy}' вакансий не найдено")
+                exit()
+            else:
+                for i in data:
+                    vacancy = Vacancy(i['vacancy'], i['company'], i['salary'], i['employment'], i['experience'],
+                                    i['requirements'], i['url'], i['platform'])
+                    if vacancy.employment in emp_criterion:
+                        vacancies.append(vacancy)
+            return vacancies
+
+    def get_vacancies_by_experience(self, experience):
+        """Возвращает вакансии, соответствующие выбранному типу занятости"""
+        if experience == 1:
+            exp_criterion = ['Нет опыта', 'Без опыта']
+        elif experience == 2:
+            exp_criterion = ['От 1 года', 'От 1 года до 3 лет']
+        elif experience == 3:
+            exp_criterion = ['От 3 до 6 лет', 'От 3 лет']
+        elif experience == 4:
+            exp_criterion = ['Более 6 лет', 'От 3 лет']
+        vacancies = []
+        with open(self.file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if not data:
+                print(f"По запросу '{self.vacancy}' вакансий не найдено")
+                exit()
+            else:
+                for i in data:
+                    vacancy = Vacancy(i['vacancy'], i['company'], i['salary'], i['employment'], i['experience'],
+                                    i['requirements'], i['url'], i['platform'])
+                    if vacancy.experience in exp_criterion:
+                        vacancies.append(vacancy)
+            return vacancies
+
     def delete_vacancy(self, vacancy):
         """Удаляет вакансию"""
         with open(self.file, 'r', encoding='utf-8') as file:
