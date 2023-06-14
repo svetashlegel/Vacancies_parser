32a5802c9755701fc0f88bc536c0202fcff561ed
shlegel <sveta.shlegel@mail.ru>
2023-06-15 00:04:23 +0300

    added function to class JSONSever

--- src/sever_cls.py
+++ src/sever_cls.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import json
+from src.vacancy_cls import Vacancy
 
 
 class VacanciesSever(ABC):
@@ -40,7 +41,24 @@ class JSONSever(VacanciesSever):
 
     def get_vacancies_by_salary(self, salary):
         """Возвращает вакансии, соответствующие уровню зарплаты"""
-        pass
+        vacancies = []
+        salary_min = int(salary.split('-')[0])
+        salary_max = int(salary.split('-')[1])
+        with open(self.file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if not data:
+                print(f"По запросу '{self.vacancy}' вакансий не найдено")
+                exit()
+            else:
+                for i in data:
+                    vacancy = Vacancy(i['vacancy'], i['company'], i['salary'], i['employment'], i['experience'],
+                                        i['requirements'], i['url'], i['platform'])
+                    if salary_min <= vacancy.salary <= salary_max:
+                        vacancies.append(vacancy)
+        if not vacancies:
+            print("В соответствии с установленным уровнем заработной платы вакансий не найдено")
+        else:
+            return vacancies
 
     def delete_vacancy(self, vacancy):
         """Удаляет вакансию"""
