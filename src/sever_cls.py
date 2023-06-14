46969325e23380944685ba1db9b21e5f301eaf66
shlegel <sveta.shlegel@mail.ru>
2023-06-14 22:55:41 +0300

    added 2 functions to class Vacancy

--- src/sever_cls.py
+++ src/sever_cls.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import json
 
 
 class VacanciesSever(ABC):
@@ -23,9 +24,19 @@ class VacanciesSever(ABC):
 class JSONSever(VacanciesSever):
     """Класс для работы с json-файлами"""
 
+    def __init__(self, vacancy_name):
+        self.file = f'{vacancy_name}.json'
+
+        with open(self.file, 'w', encoding='utf-8') as file:
+            json.dump([], file)
+
     def add_vacancy(self, vacancy):
         """Добавляет вакансию в файл"""
-        pass
+        with open(self.file, 'r', encoding='utf-8') as file:
+            data_vacancies = json.load(file)
+            data_vacancies.append(vacancy.__dict__)
+        with open(self.file, 'w', encoding='utf-8') as file:
+            json.dump(data_vacancies, file)
 
     def get_vacancies_by_salary(self, salary):
         """Возвращает вакансии, соответствующие уровню зарплаты"""
@@ -33,4 +44,14 @@ class JSONSever(VacanciesSever):
 
     def delete_vacancy(self, vacancy):
         """Удаляет вакансию"""
-        pass
+        with open(self.file, 'r', encoding='utf-8') as file:
+            data_vacancies = json.load(file)
+            counter = 0
+            for item in data_vacancies:
+                if item['vacancy'] == vacancy.vacancy:
+                    data_vacancies.pop(counter)
+                else:
+                    counter += 1
+
+        with open(self.file, 'w', encoding='utf-8') as file:
+            json.dump(data_vacancies, file)
